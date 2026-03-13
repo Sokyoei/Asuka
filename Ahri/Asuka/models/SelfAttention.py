ba289ca90d032ba2d5047e57638e0e9fdb279cc6
Sokyoei <89757286+Sokyoei@users.noreply.github.com>
2026-03-13 17:51:48 +0800

    update SelfAttention

--- Ahri/Asuka/models/SelfAttention.py
+++ Ahri/Asuka/models/SelfAttention.py
@@ -6,20 +6,25 @@ from torch import Tensor, nn
 
 
 class SelfAttention(nn.Module):
+    r"""
+    $$
+        Attention(Q, K, V) = softmax(\frac{QK^T}{\sqrt{d_k}})V
+    $$
+    """
 
     def __init__(self, dim: int, dk: int, dv: int) -> None:
         super().__init__()
-        self.scale = dk**-0.5
-        self.q = nn.Linear(dim, dk)  # queries
-        self.k = nn.Linear(dim, dk)  # keys
-        self.v = nn.Linear(dim, dv)  # values
+        self.scale = dk**-0.5  # 缩放因子
+        self.q = nn.Linear(dim, dk)  # queries 查询
+        self.k = nn.Linear(dim, dk)  # keys    关键词
+        self.v = nn.Linear(dim, dv)  # values  值
 
-    def forward(self, x):
+    def forward(self, x: Tensor):
         q: Tensor = self.q(x)
         k: Tensor = self.k(x)
         v: Tensor = self.v(x)
 
-        attn: Tensor = (q @ k.transpose(-2, -1)) * self.scale
-        attn = attn.softmax(dim=-1)
-        out = attn @ v
+        attn: Tensor = (q @ k.transpose(-2, -1)) * self.scale  # 计算相似度
+        attn = attn.softmax(dim=-1)  # 归一化
+        out = attn @ v  # 加权求和
         return out
