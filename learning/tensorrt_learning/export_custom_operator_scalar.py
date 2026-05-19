6d4bc424b6c84ed998669704d823c8f6b16f1bc8
Sokyoei <89757286+Sokyoei@users.noreply.github.com>
2026-05-19 16:56:50 +0800

    mv tensorrt_learn to learning

--- learning/tensorrt_learning/export_custom_operator_scalar.py
+++ learning/tensorrt_learning/export_custom_operator_scalar.py
@@ -1,11 +1,10 @@
-import numpy as np
 import onnx
 import onnxruntime as ort
 import onnxsim
 import torch
 from numpy.typing import NDArray
 from onnxruntime_extensions import PyCustomOpDef, onnx_op
-from onnxruntime_extensions import get_library_path as _lib_path
+from onnxruntime_extensions import get_library_path as _lib_path  # noqa: F401
 from torch import Tensor, nn
 from torch.autograd.function import Function, FunctionCtx
 from torch.onnx._internal import jit_utils
